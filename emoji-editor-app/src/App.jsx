3cabd3c2ff005a444610546b7fc53c164f041423
khushiy05 <khushiyewale150@gmail.com>
2025-10-14 18:29:46 +0530

    css

--- emoji-editor-app/src/App.jsx
+++ emoji-editor-app/src/App.jsx
@@ -3,10 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1 className="header">EMOTIFY</h1>
-    </div>
-  )
+  <div className="min-h-screen w-full bg-white relative">
+  {/* Pink Glow Background */}
+  <div
+    className="absolute inset-0 z-0"
+    style={{
+      backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, #ec4899 100%)
+      `,
+      backgroundSize: "100% 100%",
+    }}
+  />
+  <h1 className="header">EMOTIFY</h1>
+</div>
+);
 }
 export default App
 
